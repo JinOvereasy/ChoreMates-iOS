2e2642c93d7d4c084fc01d15b4067b3ed11833af
Jin <imreallyjin@gmail.com>
2017-07-12 10:18:21 -0700

    Working on Log in
    so far, I'm able to hard code in the email of the person I want to invite to the app, with hardcoded password, which I plan to ask the user to change.
    
    I need to have the email change according to the emailLabel.text as an input parameter for the method

---
+++ ChoreMates/TTTTrialViewController.swift
@@ -0,0 +1,97 @@
+//
+//  TTTTrialViewController.swift
+//  ChoreMates
+//
+//  Created by James Kang on 7/11/17.
+//  Copyright © 2017 James Kang. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuthUI
+import MessageUI
+
+class TTTTrialViewController: UIViewController, MFMailComposeViewControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func onSendVerification(_ sender: UIButton) {
+        let email = "imreallyjin@gmail.com"
+        let randomPassword = "1234567"
+        
+        Auth.auth().createUser(withEmail: email, password: randomPassword) { (user, error) in
+            if error == nil {
+                let mailComposeViewController = self.configuredMailComposeViewController()
+                if MFMailComposeViewController.canSendMail() {
+                    self.present(mailComposeViewController, animated: true, completion: nil)
+                } else {
+                    self.showSendMailErrorAlert()
+                }
+            } else {
+                print(error?.localizedDescription)
+            }
+        }
+    }
+    
+    func configuredMailComposeViewController() -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self // Extremely important to set the --mailComposeDelegate-- property, NOT the --delegate-- property
+        
+        mailComposerVC.setToRecipients(["imreallyjin@gmail.com"])
+        mailComposerVC.setSubject("Sending you an in-app e-mail...")
+        mailComposerVC.setMessageBody("Your password: 1234567", isHTML: false)
+        
+        return mailComposerVC
+    }
+    
+    func showSendMailErrorAlert() {
+        let sendMailErrorAlert = UIAlertView(title: "Could Not Send Email", message: "Your device could not send e-mail.  Please check e-mail configuration and try again.", delegate: self, cancelButtonTitle: "OK")
+        sendMailErrorAlert.show()
+    }
+    
+    // MARK: MFMailComposeViewControllerDelegate Method
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func onSignIn(_ sender: UIButton) {
+//        let email = emailTexField.text
+//        let password = passTextField.text
+        Auth.auth().signIn(withEmail: "imreallyjin@gmail.com", password: "1234567") { (user, error) in
+            print(user, error)
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
