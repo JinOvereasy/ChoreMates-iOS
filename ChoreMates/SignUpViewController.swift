4bd69d80cde3bc9b6db85b9f485a593f0e6a52ce
Jin <imreallyjin@gmail.com>
2017-07-13 14:58:13 -0700

    It compiles, but
    I need to move all of the functioning Log In into the Main.storyboard, keep them in 1 storyboard for them to work

--- ChoreMates/SignUpViewController.swift
+++ ChoreMates/SignUpViewController.swift
@@ -7,12 +7,62 @@
 //
 
 import UIKit
+import Firebase
 
 class SignUpViewController: UIViewController {
 
+    // MARK: - IBOutlets
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    
+    // MARK: - IBActions
+    @IBAction func registerAccount(sender: UIButton) {
+        // Validate the input
+        guard let name = nameTextField.text, name != "",
+        let emailAddress = emailTextField.text, emailAddress != "",
+        let password = passwordTextField.text, password != "" else {
+            
+            let alertController = UIAlertController(title: "Registration Error", message: "Please make sure you provide your name, email address and password to complete the registration.", preferredStyle: .alert)
+            let okayAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(okayAction)
+            present(alertController, animated: true, completion: nil)
+            
+            return
+        }
+        
+        // Register the user account on Firebase
+        Auth.auth().createUser(withEmail: emailAddress, password: password, completion: { (user, error) in
+        
+            if let error = error {
+                let alertController = UIAlertController(title: "Registration Error", message: error.localizedDescription, preferredStyle: .alert)
+                let okayAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                alertController.addAction(okayAction)
+                self.present(alertController, animated: true, completion: nil)
+                
+                return
+            }
+            
+            // Save the name of the user
+            if let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest() {
+                changeRequest.displayName = name
+                changeRequest.commitChanges(completion: { (error) in
+                    if let error = error {
+                        print("Failed to change the display name: \(error.localizedDescription)")
+                    }
+                })
+            }
+            // Dismiss keyboard
+            self.view.endEditing(true)
+            
+            // Present the main view
+            if let viewController = self.storyboard?.instantiateViewController(withIdentifier: "MainView") {
+                UIApplication.shared.keyWindow?.rootViewController = viewController
+                self.dismiss(animated: true, completion: nil)
+            }
+            
+        })
+    }
     
     
     override func viewDidLoad() {
@@ -28,14 +78,34 @@ class SignUpViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
